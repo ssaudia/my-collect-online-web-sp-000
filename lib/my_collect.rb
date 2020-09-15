79a496c3b22af1df85037263d1cf988074b75c55
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-15 01:59:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,4 @@
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
 end
-
-# => ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]
